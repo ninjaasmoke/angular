4ed514475929baab33075fd58866f90200533737
Nithin Sai K J <nithins674@gmail.com>
2020-08-16 23:21:17 +0530

    assignment 2

---
+++ module-2-assignment/js/app.js
@@ -0,0 +1,101 @@
+(
+    function () {
+        'use strict';
+
+        angular.module("ShoppinhListCheckOff", [])
+            .controller("ToBuyController", ToBuy)
+            .controller("AlreadyBoughtController", AlreadyBought)
+            .service("ShoppingListCheckOffService", ShoppingListCheckOff)
+            ;
+
+        ToBuy.$inject = ["ShoppingListCheckOffService"];
+
+        function ToBuy(ShoppingListCheckOffService) {
+            var toBuy = this;
+
+            toBuy.itemName = "";
+            toBuy.itemQuantity = "";
+
+            toBuy.addItem = function () {
+                ShoppingListCheckOffService.buyItem(toBuy.itemName, toBuy.itemQuantity);
+                checkBought();
+            }
+
+            toBuy.bought = function (index) {
+                ShoppingListCheckOffService.boughtItem(index);
+                checkBought();
+            }
+
+            toBuy.items = ShoppingListCheckOffService.getToBuyItems();
+
+            function checkBought() {
+                if (toBuy.items.length == 0) {
+                    toBuy.show = true;
+                }
+                else {
+                    toBuy.show = false;
+                }
+            }
+
+        }
+
+
+        AlreadyBought.$inject = ["ShoppingListCheckOffService"];
+
+        function AlreadyBought(ShoppingListCheckOffService) {
+            var alreadyBought = this;
+
+            alreadyBought.getBoughtItems = ShoppingListCheckOffService.getBoughtItems();
+
+            alreadyBought.removeBoughtItems = function (index) {
+                ShoppingListCheckOffService.removeBought(index);
+            }
+
+            alreadyBought.boughtAtleastOne = function () {
+                return ShoppingListCheckOffService.bought();
+            }
+
+        }
+
+
+        function ShoppingListCheckOff() {
+            var service = this;
+
+            var toBuy = [];
+            var alreadyBought = [];
+
+            service.bought = function () {
+                return false;
+            };
+
+            service.buyItem = function (itemName, quantity) {
+                var item = {
+                    name: itemName,
+                    quantity: quantity
+                };
+                toBuy.push(item);
+            };
+
+            service.getToBuyItems = function () {
+                return toBuy;
+            };
+
+            service.getBoughtItems = function () {
+                return alreadyBought;
+            }
+
+            service.boughtItem = function (indexId) {
+                service.bought = function () {
+                    return true;
+                };
+                alreadyBought.push(toBuy[indexId]);
+                toBuy.splice(indexId, 1);
+            };
+
+            service.removeBought = function (indexId) {
+                alreadyBought.splice(indexId, 1);
+            }
+        }
+
+    }
+)();
